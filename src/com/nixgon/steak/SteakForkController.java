7a59565e16e17c2efb5935073affaf62b701d716
kimdata <kimdatagoon@gmail.com>
2014-06-13 17:53:19 +0900

    회의 후 새로운 시작

--- src/com/nixgon/steak/SteakForkController.java
+++ src/com/nixgon/steak/SteakForkController.java
@@ -1,25 +1,14 @@
 package com.nixgon.steak;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
 import java.util.logging.Logger;
-
-import javax.jdo.PersistenceManager;
-import javax.jdo.Query;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
-import com.nixgon.steak.model.SteakModel;
-import com.nixgon.steak.model.SteakPipelineModel;
-import com.nixgon.steak.model.SteakStageModel;
 
 @Controller
 public class SteakForkController {
@@ -41,6 +30,8 @@ public class SteakForkController {
 	public String steak( ModelMap model ) {
 		UserService userService = UserServiceFactory.getUserService();
 		User user = userService.getCurrentUser();
+		
+		
 
 		return "steak";
 	}
